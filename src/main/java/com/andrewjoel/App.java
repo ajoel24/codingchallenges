dd729758f9a1f379d3fc5e895581077bbd528a3e
ajoel24 <drew.devops@gmail.com>
2022-03-26 10:35:54 +0530

    Update logger

--- src/main/java/com/andrewjoel/App.java
+++ src/main/java/com/andrewjoel/App.java
@@ -3,9 +3,6 @@ package com.andrewjoel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Hello world!
- */
 public class App {
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
